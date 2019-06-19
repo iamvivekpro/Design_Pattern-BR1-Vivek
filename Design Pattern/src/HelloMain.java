9eba4ebc7d063cd6d5eb607e02fac5f37e8677b0
vivek.kumar <vivek.kumar@CNBNG-LT1046.Corp.irdeto.com>
2019-06-19 23:37:55 +0530

    Vivek

--- Design Pattern/src/HelloMain.java
+++ Design Pattern/src/HelloMain.java
@@ -3,6 +3,7 @@ public class HelloMain {
 	public static void main(String []a) {
 		System.out.println("Hello World 11");
 		HelloMainDemo x= new HelloMainDemo();
+		System.out.println("Hello");
 		System.out.println(x.sqareQ(55));
 		
 	}
