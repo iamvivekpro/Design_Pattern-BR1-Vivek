804895d1e8d3f557c9ea1420580b0c0980b35c02
vivek.kumar <vivek.kumar@CNBNG-LT1046.Corp.irdeto.com>
2019-07-23 13:41:02 +0530

    Error Commited

--- Design Pattern/src/HelloMain.java
+++ Design Pattern/src/HelloMain.java
@@ -2,7 +2,6 @@
 public class HelloMain {
 	public static void main(String []a) {
 		System.out.println("Hello World 11");
-		System.out.println("Vivek");
-		
+		System.out.println("Vivek");aadadad
 	}
 }
